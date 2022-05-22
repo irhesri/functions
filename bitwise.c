cd6c533a4a18087bc227ddb5e815e4aba900e64b
Imane Rhesri <irhesri@e2r13p5.1337.ma>
2022-05-22 18:11:40 +0100

    ..

---
+++ bitwise.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+
+void	print_bits(unsigned char u)
+{
+	int	i;
+	int	b;
+
+	i = 8;
+	while (i--)
+	{
+		b = ((u & 128) != 0) + 48;
+		write(1, &b, 1);
+		u <<= 1;
+	}
+}
+
+unsigned char	reverse_bits(unsigned char c)
+{
+	unsigned char	u;
+	int				i;
+
+	u = 0;
+	i = 8;
+	while (i--)
+	{
+		u <<= 1;
+		u |= (c & 1);
+		c >>= 1;
+	}
+	return (u);
+}
+
+// int	main()
+// {
+// 	print_bits('2');
+// 	printf("\n");
+// 	print_bits(reverse_bits('2'));
+// 	printf("\n");
+// 	printf("%c\n", reverse_bits('2'));
+// }
